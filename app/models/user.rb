96543b19d658438c60ec65a3122bef8571f0c671
StevenTran91 <sdtran91@gmail.com>
2021-06-01 18:35:07 -0700

    heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :first_name, :last_name, :password_digest, presence: true
-    validates :email, :session_token, :presence: true, uniqueness: true 
+    validates :email, :session_token, presence: true, uniqueness: true 
     validates :password, length: { minimum: 6, allow_nil: true}
     after_initialize :ensure_session_token
     attr_reader :password
