94d5d50b21991b4c1654719702db996617920cc1
StevenTran91 <sdtran91@gmail.com>
2021-06-03 14:02:09 -0700

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(
+    first_name: "",
+    last_name: "",
+    email: "user@user.com",
+    password: "123456",
+    month: "Jan",
+    day: "1",
+    year: "2021",
+    gender: "",
+)
+
+
