ed42f7aa4850d26bd42eb076f704faed8cc5c4f8
StevenTran91 <sdtran91@gmail.com>
2021-06-04 10:38:14 -0700

    forgot to uncomment LoginFormContainer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,7 +13,7 @@ const App = () => {
         <div>   
             <Modal />
             <NavbarContainer />
-            {/* <LoginFormContainer /> */}
+            <LoginFormContainer />
 
             <Switch>
 
