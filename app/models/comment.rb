d11df115f75af8daf14533d75820f18d8ef7630a
StevenTran91 <sdtran91@gmail.com>
2021-07-14 12:30:03 -0700

    add likes association to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,17 @@
 class Comment < ApplicationRecord
     validates :body, :post_id, :commenter_id, presence: true 
 
+
     belongs_to :post, 
     foreign_key: :post_id, 
     class_name: :Post 
 
     belongs_to :author, 
+    primary_key: :id,
     foreign_key: :commenter_id,
     class_name: :User 
+
+    has_many :likes, dependent: :destroy
 
     
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,10 +2,12 @@ class Friendship < ApplicationRecord
     validates :user_id, :friend_id, presence: true
 
     belongs_to :user,
+    primary_key: :id,
     foreign_key: :user_id,
     class_name: :User 
 
     belongs_to :friend,
+    primary_key: :id, 
     foreign_key: :friend_id,
     class_name: :User 
 
